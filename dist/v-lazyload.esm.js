aa86b892cc44b34579abd1b1a78f8a2e5b7cf28a
Yvan Yang <i.yvanyang@gmail.com>
2019-07-18 11:10:47 +0800

    build

--- dist/v-lazyload.esm.js
+++ dist/v-lazyload.esm.js
@@ -11,40 +11,40 @@ function () {
   function LazyloadManager() {
     this.rafId = null;
     this.els = new Set();
-    this.handleViewportChange = this.handleViewportChange.bind(this);
+    this.handleIntersectionChangeLegacy = this.handleIntersectionChangeLegacy.bind(this);
+    this.handleIntersectionChange = this.handleIntersectionChange.bind(this);
+    this.intersectionObserver = new IntersectionObserver(this.handleIntersectionChange);
   }
 
   var _proto = LazyloadManager.prototype;
 
   _proto.add = function add(el) {
     this.els.add(el);
-    this.checkEl(el);
+    this.changeClass(el);
+    this.intersectionObserver.observe(el);
+    this.checkIntersectionLegacy(el);
 
     if (this.els.size === 1) {
-      window.addEventListener('scroll', this.handleViewportChange);
-      window.addEventListener('resize', this.handleViewportChange);
+      window.addEventListener('scroll', this.handleIntersectionChangeLegacy);
+      window.addEventListener('resize', this.handleIntersectionChangeLegacy);
     }
   };
 
   _proto.remove = function remove(el) {
     this.els["delete"](el);
+    this.intersectionObserver.unobserve(el);
 
     if (this.els.size === 0) {
-      window.removeEventListener('scroll', this.handleViewportChange);
-      window.removeEventListener('resize', this.handleViewportChange);
+      window.removeEventListener('scroll', this.handleIntersectionChangeLegacy);
+      window.removeEventListener('resize', this.handleIntersectionChangeLegacy);
     }
   };
 
-  _proto.checkEl = function checkEl(el) {
-    var _el$dataset = el.dataset,
-        loadingSrc = _el$dataset.loading,
-        src = _el$dataset.src;
-
-    if (!src) {
+  _proto.checkIntersectionLegacy = function checkIntersectionLegacy(el) {
+    if (!el.dataset.src) {
       return;
     }
 
-    var changeStateClass = this.changeClass.bind(this, el);
     var winHeight = window.screen.height;
     var viewHeight = window.innerHeight;
 
@@ -52,45 +52,70 @@ function () {
         top = _el$getBoundingClient.top,
         bottom = _el$getBoundingClient.bottom;
 
-    var inView = top >= 0 && top < viewHeight || bottom >= 0 && bottom < viewHeight;
     var inPrevView = bottom > -winHeight && bottom < 0;
     var inNextView = top > viewHeight && top < viewHeight + winHeight;
     var inSiblingView = inPrevView || inNextView;
 
     if (inSiblingView) {
-      if (loadingSrc) {
-        el.src = loadingSrc;
-      }
-    } else if (inView) {
-      var img = new Image();
-      img.addEventListener('load', function () {
-        changeStateClass(STATE_CLASS.LOADED);
-        el.src = src;
-      }, {
-        once: true
-      });
-      img.addEventListener('error', function () {
-        changeStateClass(STATE_CLASS.ERROR);
-        el.src = src;
-      }, {
-        once: true
-      });
-      changeStateClass(STATE_CLASS.LOADING);
-      img.src = src;
-      this.remove(el);
+      this.loadLoadingImg(el);
     }
   };
 
-  _proto.handleViewportChange = function handleViewportChange() {
+  _proto.checkIntersection = function checkIntersection(intEntry) {
+    if (intEntry.intersectionRatio) {
+      this.loadOriginImg(intEntry.target);
+    }
+  };
+
+  _proto.loadLoadingImg = function loadLoadingImg(el) {
+    var loading = el.dataset.loading;
+
+    if (loading) {
+      el.src = loading;
+    }
+  };
+
+  _proto.loadOriginImg = function loadOriginImg(el) {
     var _this = this;
+
+    var src = el.dataset.src;
+    var img = new Image();
+    img.addEventListener('load', function () {
+      _this.changeClass(el, STATE_CLASS.LOADED);
+
+      el.src = src;
+    }, {
+      once: true
+    });
+    img.addEventListener('error', function () {
+      _this.changeClass(el, STATE_CLASS.ERROR);
+
+      el.src = src;
+    }, {
+      once: true
+    });
+    this.changeClass(el, STATE_CLASS.LOADING);
+    img.src = src;
+  };
+
+  _proto.handleIntersectionChangeLegacy = function handleIntersectionChangeLegacy() {
+    var _this2 = this;
 
     if (this.rafId) return;
     this.rafId = requestAnimationFrame(function () {
-      _this.els.forEach(function (el) {
-        return _this.checkEl(el);
+      _this2.els.forEach(function (el) {
+        return _this2.checkIntersectionLegacy(el);
       });
 
-      _this.rafId = null;
+      _this2.rafId = null;
+    });
+  };
+
+  _proto.handleIntersectionChange = function handleIntersectionChange(intEntries) {
+    var _this3 = this;
+
+    intEntries.forEach(function (intEntry) {
+      return _this3.checkIntersection(intEntry);
     });
   };
 
